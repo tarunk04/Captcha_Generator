648afe7b312dc347f9be467a15b9437e541b955f
Tarun <tarun12.tarunkr@gmail.com>
2017-06-11 19:08:04 +0530

    Adding comments

--- captcha.php
+++ captcha.php
@@ -1,14 +1,12 @@
 <?php
-session_start();
+//GENERATING RANDOM 4 CHARACTER FOR CAPTCHA
+  $string='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhijklmnopqrstuvwxyz1234567890';
+  $string_shuff=str_shuffle($string);
+  $text=substr($string_shuff,0,4);
 
+//STARTING AND CREATING SESSION
+  session_start();
+  $_SESSION['secure']=$text;
 
-$string='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefhijklmnopqrstuvwxyz1234567890';
-$string_shuff=str_shuffle($string);
-$text=substr($string_shuff,0,4);
-
-
-
-$_SESSION['secure']=$text;
-
- ?>
- <img src="Captcha.php" alt="">
+?>
+<img src="img_gen.php">
--- img_gen.php
+++ img_gen.php
@@ -1,45 +1,57 @@
 <?php
-session_start();
-header('content-type: image/jpeg');
-$text=$_SESSION['secure'];
-$font_size=rand(22,27);
+//STARTING  SESSION
+  session_start();
+//DEFINING CONTENT TYPE TO IMAGE - JPEG
+  header('content-type: image/jpeg');
+//GETTING SESSION VARIABLE
+  $text=$_SESSION['secure'];
 
-$image_height=60;
-$image_width=(32*4)+30;
-
-$image = imagecreate($image_width, $image_height);
-
-imagecolorallocate($image, 255 ,255, 255);
-
-
-
-for ($i=1; $i<=4;$i++){
-$font_size=rand(22,27);
-$r=rand(0,255);
-$g=rand(0,255);
-$b=rand(0,255);
-$index=rand(1,10);
-$x=15+(30*($i-1));
-$x=rand($x-5,$x+5);
-$o=rand(-30,30);
-$y=rand(35,45);
-$font_color = imagecolorallocate($image, $r ,$g, $b);
-imagettftext($image, $font_size, $o, $x, $y ,  $font_color,'fonts/'.$index.'.ttf',$text[$i-1]);
-}
-for($i=1; $i<=30;$i++){
-$x1= rand(1,150);
-$y1= rand(1,150);
-$x2= rand(1,150);
-$y2= rand(1,150);
-$r=rand(0,255);
-$g=rand(0,255);
-$b=rand(0,255);
-
-$font_color = imagecolorallocate($image, $r ,$g, $b);
-imageline($image,$x1,$y1,$x2,$y2,$font_color);
+//CREATING IMAGE WITH DIMENTION 158x60
+  $image_height=60;
+  $image_width=(32*4)+30;
+  $image = imagecreate($image_width, $image_height);
+  //DEFINING BACKGROUND COLOUR TO WHITE
+  imagecolorallocate($image, 255 ,255, 255);
 
 
-}
-imagejpeg($image);
+//FOR LOOP FOR CREATING TEXT 
+  for ($i=1; $i<=4;$i++){
+    //CREATING RANDOM FONT-SIZE
+      $font_size=rand(22,27);
+    //FOR RANDOM COLOUR
+      $r=rand(0,255);
+      $g=rand(0,255);
+      $b=rand(0,255);
+    //RANDOM INDEX FOR RANDOM TEXT FONT
+      $index=rand(1,10);
+    //RANDOM POSITION AND ORIANTION
+      $x=15+(30*($i-1));
+      $x=rand($x-5,$x+5);
+      $y=rand(35,45);
+      $o=rand(-30,30);
+    //RANDOM FONT COLOR
+      $font_color = imagecolorallocate($image, $r ,$g, $b);
+    //CREATING IMAGE USING DIFFETENT FONTS
+      imagettftext($image, $font_size, $o, $x, $y ,  $font_color,'fonts/'.$index.'.ttf',$text[$i-1]);
+  }
+//FOR LOOP FOR CREATING RANDOM LINES
+  for($i=1; $i<=30;$i++){
+    //RANDOM STARTING AND ENDING POSITION 
+      $x1= rand(1,150);
+      $y1= rand(1,150);
+      $x2= rand(1,150);
+      $y2= rand(1,150);
+    //RANDOM COLOR
+      $r=rand(0,255);
+      $g=rand(0,255);
+      $b=rand(0,255);
+      $font_color = imagecolorallocate($image, $r ,$g, $b);
+    //CREATING RANDOM LINES
+      imageline($image,$x1,$y1,$x2,$y2,$font_color);
+
+
+  }
+//CREATING FINAL IMAGE (CAPTCHA)
+  imagejpeg($image);
 
 ?>
